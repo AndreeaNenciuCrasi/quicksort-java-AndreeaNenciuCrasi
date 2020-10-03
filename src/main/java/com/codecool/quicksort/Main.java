33afba290db545f16d8df34864630e3865326e1e
AndreeaNenciuCrasi <aanicaa@yahoo.com>
2020-10-03 16:54:41 +0300

    Adds quicksort

--- src/main/java/com/codecool/quicksort/Main.java
+++ src/main/java/com/codecool/quicksort/Main.java
@@ -1,9 +1,18 @@
 package com.codecool.quicksort;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-        // Use the tests to check your code!
+        List<Integer> intArray = Arrays.asList(20, 35, -15, 7, 55, 1, -22);
+
+        QuickSort.sort(intArray);
+
+        for (int i = 0; i < intArray.size(); i++) {
+            System.out.println(intArray.get(i));
+        }
     }
 
 }
--- src/main/java/com/codecool/quicksort/QuickSort.java
+++ src/main/java/com/codecool/quicksort/QuickSort.java
@@ -1,5 +1,6 @@
 package com.codecool.quicksort;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class QuickSort {
@@ -8,7 +9,39 @@ public class QuickSort {
      * Sorts the given List in place
      * @param toSort the List to sort. Throws an error if its null
      */
-    public void sort(List<Integer> toSort) {
+    public static void sort(List<Integer> toSort) {
+        Integer[] intArray = (Integer[]) toSort.toArray();
+        quickSort(intArray, 0, intArray.length);
+        toSort = Arrays.asList(intArray);
+    }
 
+    public static void quickSort(Integer[] input, int start, int end) {
+        if (end - start < 2) {
+            return;
+        }
+        int pivotIndex = partition(input, start, end);
+        quickSort(input, start, pivotIndex);
+        quickSort(input, pivotIndex+1, end);
+    }
+
+    public static int partition(Integer[] input, int start, int end) {
+        //This is using the first element as the pivot
+        Integer pivot = input[start];
+        int i = start;
+        int j = end;
+
+        while (i < j) {
+            //NOTE: Empty loop body
+            while (i < j && input[--j] >= pivot);
+            if (i < j) {
+                input[i] = input[j];
+            }
+            while (i < j && input[++i] <= pivot);
+            if (i < j) {
+                input[j] = input[i];
+            }
+        }
+        input[j] = pivot;
+        return j;
     }
 }
